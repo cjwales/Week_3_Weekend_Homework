b0227316e57ca2eafeebfa2ce6c692c49163c6b4
cjwales <cjwales@gmail.com>
2019-09-13 11:54:26 +0100

    added crud to customer and film

--- db/codeclan_cinema.sql
+++ db/codeclan_cinema.sql
@@ -5,19 +5,19 @@ DROP TABLE customers;
 
 
 CREATE TABLE films(
-  id SERIAL4 INT4 PRIMARY KEY,
+  id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
   price INT4
 );
 
 CREATE TABLE customers(
-  id SERIAL4 INT4 PRIMARY KEY,
+  id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
   funds INT4
 );
 
 CREATE TABLE tickets(
-  id SERIAL4 INT4 PRIMARY KEY,
+  id SERIAL4 PRIMARY KEY,
   customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
   film_id INT4 REFERENCES films(id) ON DELETE CASCADE
 );
